12f02f2806b1df32bc6afc4b1f7cb9862f778ec8
Eliza5010 <elizaelizahanje@gmail.com>
2022-09-19 14:15:03 +0300

    to add any number

---
+++ while-loop/do-while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+int num,sum=0;
+do{
+printf("inter numbers;");
+scanf("%d",&num);
+sum+=num;
+}
+while(num !=0);
+{
+printf("sum of all noted is %d", sum);
+}
+return 0;
+}
+
+
